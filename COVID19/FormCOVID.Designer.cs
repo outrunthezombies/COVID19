06186c1f23c2bfa12abeda977f224554ae124185
Greg Salsman <gbsalsman@gmail.com>
2020-04-03 19:58:59 -0400

    Add project files.

---
+++ COVID19/FormCOVID.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace COVID19
+{
+    partial class FormCOVID
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormCOVID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormCOVID";
+            this.Text = "COVID19 Analysis";
+            this.Load += new System.EventHandler(this.FormCOVID_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ COVID19/FormCOVID.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace COVID19
+{
+    public partial class FormCOVID : Form
+    {
+        public FormCOVID()
+        {
+            InitializeComponent();
+        }
+        private string CovidJSONURL = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json/";
+        private string CovidJSONRawData = "";
+
+        private void FormCOVID_Load(object sender, EventArgs e)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                CovidJSONRawData = webClient.DownloadString(CovidJSONURL);
+            }
+        }
+    }
+}
